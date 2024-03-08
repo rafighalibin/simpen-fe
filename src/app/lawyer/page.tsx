e1be5fb7a527699285838bee53071c398530231d
Rafi Ghalibin Abrar <rafi.ghalibin@ui.ac.id>
2024-03-08 19:19:02 +0700

    Rafighalibin/experiment

--- src/app/lawyer/page.tsx
+++ src/app/lawyer/page.tsx
@@ -1,15 +1,12 @@
 "use client";
-import { useQuery } from "react-query";
 import React from "react";
-import { AddForm } from "../../components/LawyerPage/addForm";
-import useFetchWithToken from "../../common/hooks/fetchWithToken";
-import { Lawyer } from "../../common/types/lawyer";
-import { LawyerTable } from "../../components/LawyerPage/table";
+import { LawyerForm } from "../../components/LawyerPage/lawyerForm";
+import { LawyerTable } from "../../components/LawyerPage/lawyerTable";
 
 export default function Page() {
   return (
     <div className="overflow-x-auto">
-      <AddForm />
+      <LawyerForm />
       <LawyerTable />
     </div>
   );
