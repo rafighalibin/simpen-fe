3367a437ffe22d1204e2ae9e253ad2ec7cb931fd
Kristo Jeremy Thady Tobing <kristo.jeremy@ui.ac.id>
2024-03-12 01:49:15 +0700

    Finished: logout

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,27 @@
 "use client";
 
-import { QueryClient, QueryClientProvider } from "react-query";
+import { QueryClient } from "react-query";
 import { LoginForm } from "../../components/loginPage/loginForm";
+import { Background } from "../../components/loginPage/background";
+
+// Import images
 
 const queryClient = new QueryClient();
 
 export default function App() {
-  return <LoginForm />;
+  return (
+    <div>
+      <div style={{ position: "relative", height: "100vh" }}>
+        <Background />
+        <div
+          className="z-10 flex items-center justify-center h-full"
+          style={{ position: "absolute", top: 0, left: 0, right: 0, bottom: 0 }}
+        >
+          <div className="max-w-xl w-full space-y-8">
+            <LoginForm />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
